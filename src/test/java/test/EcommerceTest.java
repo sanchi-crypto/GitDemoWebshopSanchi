cc932890a511fb78704704b87807e42c68813836
sanchi <sanchipunjani@gmail.com>
2025-10-09 10:38:39 +0530

    Updated EcommerceTest.java with latest changes

--- src/test/java/test/EcommerceTest.java
+++ src/test/java/test/EcommerceTest.java
@@ -1,5 +1,12 @@
 package test;
 
 public class EcommerceTest {
+	
+	private void Sysout() {
+		// TODO Auto-generated method stub
+
+		System.out.println("HI I am Raman");
+		System.out.println("HI I am Raman punjani");
+	}
 
 }
