44d749db03be373fac0cb3f7e66c023bdd1a1edd
sanchi <sanchipunjani@gmail.com>
2025-10-09 10:46:55 +0530

    Add Architect Asian Time Zone

--- src/test/java/test/EcommerceTest.java
+++ src/test/java/test/EcommerceTest.java
@@ -8,5 +8,21 @@ public class EcommerceTest {
 		System.out.println("HI I am Raman");
 		System.out.println("HI I am Raman punjani");
 	}
+	
+	private void ADDAPPS() {
+		// TODO Auto-generated method stub
 
+		System.out.println("HI I am Appium");
+		System.out.println("HI I am JAVA");
+		System.out.println("HI I am JAVASCRIPT");
+		System.out.println("HI I am SPRINGBOOT");
+	}
+	private void AndroidAPPS() {
+		// TODO Auto-generated method stub
+
+		System.out.println("HI I am Appium");
+		System.out.println("HI I am JAVA");
+		System.out.println("HI I am JAVASCRIPT");
+		System.out.println("HI I am SPRINGBOOT");
+	}
 }
